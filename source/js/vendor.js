b79c5ca3d677c8047ac7ad60d3c2acc96ff23c5b
Маргарита Пак <paktoka@gmail.com>
2023-08-14 23:16:22 +0300

    акселератор 2 день: стиль десктоп

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -4,41 +4,26 @@ import './vendor/focus-visible-polyfill';
 
 Swiper.use([Navigation, Pagination]);
 
-const sliderTrainers = new Swiper('.trainers__slider', {
+const sliderHero = new Swiper('.hero', {
   loop: true,
-  navigation: {
-    nextEl: '.trainers__btn-next',
-    prevEl: '.trainers__btn-prev',
-  },
-
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-      slidesPerGroup: 1,
-    },
-    767: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-      slidesPerGroup: 2,
-    },
-    1199: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-      slidesPerGroup: 4,
-    },
-  },
-});
-
-const sliderReviews = new Swiper('.reviews__slider', {
-  loop: false,
-  navigation: {
-    nextEl: '.reviews__btn-next',
-    prevEl: '.reviews__btn-prev',
+  pagination: {
+    el: '.hero__pagination',
+    clickable: true,
   },
   slidesPerView: 1,
   spaceBetween: 0,
   slidesPerGroup: 1,
 });
 
-export {sliderTrainers, sliderReviews};
+const sliderTours = new Swiper('.tours', {
+  loop: true,
+  navigation: {
+    prevEl: '.tours__btn-prev',
+    nextEl: '.tours__btn-next',
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+});
+
+export {sliderHero, sliderTours};
