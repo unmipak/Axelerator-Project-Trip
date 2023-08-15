3e4c458e941a964d0dc396a311014ef379a20b97
Маргарита Пак <paktoka@gmail.com>
2023-08-15 22:16:38 +0300

    Акселератор 2 день: desktop

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -13,9 +13,10 @@ const sliderHero = new Swiper('.hero', {
   slidesPerView: 1,
   spaceBetween: 0,
   slidesPerGroup: 1,
+  speed: 300,
 });
 
-const sliderTours = new Swiper('.tours', {
+const sliderTours = new Swiper('.tours__slider', {
   loop: true,
   navigation: {
     prevEl: '.tours__btn-prev',
@@ -26,4 +27,37 @@ const sliderTours = new Swiper('.tours', {
   slidesPerGroup: 1,
 });
 
-export {sliderHero, sliderTours};
+const sliderInstructors = new Swiper('.instructors', {
+  loop: true,
+  navigation: {
+    prevEl: '.instructors__btn-prev',
+    nextEl: '.instructors__btn-next',
+  },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+});
+
+const sliderReviews = new Swiper('.reviews__slider', {
+  loop: false,
+  navigation: {
+    prevEl: '.reviews__btn-prev',
+    nextEl: '.reviews__btn-next',
+  },
+  slidesPerView: 1.48,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+});
+
+const sliderAdvantages = new Swiper('.advantages__slider', {
+  loop: true,
+  navigation: {
+    prevEl: '.advantages__btn-prev',
+    nextEl: '.advantages__btn-next',
+  },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+});
+
+export {sliderHero, sliderTours, sliderInstructors, sliderReviews, sliderAdvantages};
